2ba75951b3993b76ff04597d7b26ed062a1b17d7
bai.zixv <bai.zixv@foxmail.com>
2023-05-08 18:27:18 +0800

    dev: bai-utils, 测试模块

---
+++ src/index.js
@@ -0,0 +1,10 @@
+import baiUtils from 'bai-utils'
+
+const num = baiUtils.getDaysNum(2020, 11)
+
+console.log(num)
+console.log(
+  '%c Line:6 🍑 log',
+  'font-size:18px;color:#fca650;background:#b03734',
+  baiUtils.getRandomColor()
+)
